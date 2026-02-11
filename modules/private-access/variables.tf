15b20b9ced2c363611ed71603bb5d1ad89605ad0
doxajay <99846671+doxajay@users.noreply.github.com>
2026-02-11 12:38:00 -0700

    Add variables for private access module

---
+++ modules/private-access/variables.tf
@@ -0,0 +1,7 @@
+variable "prefix" { type = string }
+variable "location" { type = string }
+variable "environment" { type = string }
+
+variable "hub_vnet_id" { type = string }
+variable "hub_vnet_name" { type = string }
+variable "hub_resource_group" { type = string }
