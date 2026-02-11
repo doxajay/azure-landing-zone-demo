2518625309f2b0c7bfa43ee73172c63bad56bc20
doxajay <99846671+doxajay@users.noreply.github.com>
2026-02-11 12:37:44 -0700

    Update outputs in networking module
    Replaced existing outputs with new outputs for hub VNet name, hub resource group name, and private endpoints subnet ID.

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,11 +1,15 @@
-output "network_resource_group_name" {
-  value = azurerm_resource_group.net.name
-}
-
 output "hub_vnet_id" {
   value = azurerm_virtual_network.hub.id
 }
 
-output "spoke_vnet_ids" {
-  value = { for k, v in azurerm_virtual_network.spokes : k => v.id }
+output "hub_vnet_name" {
+  value = azurerm_virtual_network.hub.name
+}
+
+output "hub_rg_name" {
+  value = azurerm_resource_group.hub.name
+}
+
+output "private_endpoints_subnet_id" {
+  value = azurerm_subnet.private_endpoints.id
 }
