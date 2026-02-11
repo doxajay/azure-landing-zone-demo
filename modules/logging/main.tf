2b113dad62f01a5bdfec8aebe6de5fd26f131186
doxajay <99846671+doxajay@users.noreply.github.com>
2026-02-11 12:36:29 -0700

    Add Azure resources for logging module

---
+++ modules/logging/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "log" {
+  name     = "rg-${var.prefix}-${var.environment}-log"
+  location = var.location
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "law-${var.prefix}-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.log.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
