45118061d8bc3b4224ff1b3b5b470a315a3a0c1d
doxajay <99846671+doxajay@users.noreply.github.com>
2026-02-11 12:34:33 -0700

    Add variables for prefix and management group ID

---
+++ modules/policy/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  type        = string
+  description = "Naming prefix"
+}
+
+variable "management_group_id" {
+  type        = string
+  description = "Management group name/id (e.g. alz-platform)"
+}
